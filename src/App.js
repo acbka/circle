fac0dc013740b7001f650091fbaf51e5da11c30e
acbka <vladochka.vb@gmail.com>
2021-01-23 13:32:49 +1300

    arc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Canvas } from './components/canvas'
 
 function App() {
 
-   const color = ["b9c6ce", "6c889a", "C42F2F" ];
+   const color = ["#b9c6ce", "#6c889a", "#C42F2F" ];
 
    const member = [];
 
@@ -21,29 +21,25 @@ function App() {
       member[i] = {
          id: i,
          start: arcStep * i,
-         color: color[random(0, 2)]
+         end: arcStep * (i+1),
+         color: color[random(0, 3)]
       }
    }
 
+   
   
    
    const list = member.map((item,index) => {
-      return <li key={index} >member = {item.id}, arc = {item.start}, color = {item.color}</li>;
+      return      <Canvas key= {index} start={item.start} end={item.end} color={item.color}></Canvas>;
    })
 
+   console.log("member", member)
+console.log("member1", member[1].start)
 
   return (
    <div>
       <p>numberOfMembers = {numberOfMembers}</p>
       {list}
-      <Canvas
-      draw = {ctx => {
-         ctx.beginPath();
-         ctx.arc(95, 50, 40, 270*Math.PI/180, 0, true);
-         ctx.strokeStyle = color[1];
-         ctx.stroke();
-         }
-       }></Canvas>
    </div>
   );
 }
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,13 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect} from 'react';
+import '../css/canvas.css';
 
-export const Canvas = ({ draw }) => {
-  const canvas = React.createRef();
+export const Canvas = ({start, end, color}) => {
 
-  useEffect(() => {
-    const ctx = canvas.current.getContext('2d');
-    draw(ctx);
-  }, [draw, canvas]);
+   const startArc = (start+1)*Math.PI/180,
+      endtArc = (end-1)*Math.PI/180;
+   
+   const canvasRef = useRef(null)
 
-  return <canvas ref={canvas} />;
-};
+   let img = new Image();
+   img.src = "../img/cross.png";
 
+   useEffect(() => {
+     
+      const canvas = canvasRef.current
+      const context = canvas.getContext('2d')
+
+      const draw = ctx => {
+      ctx.beginPath()
+      ctx.lineWidth = 16
+      ctx.arc(300, 150, 120, startArc, endtArc)
+      ctx.strokeStyle = color; 
+      ctx.stroke();
+      ctx.beginPath()
+      ctx.strokeStyle = color; 
+      ctx.stroke();
+   }
+     
+     draw(context)
+
+   }, [startArc, endtArc, color])
+   
+   return <canvas className="canvas" ref={canvasRef} width="600" height="300"/>
+ }
